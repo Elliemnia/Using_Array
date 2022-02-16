cf23c3ce3370b6ce16f4892216adc89be625cd4e
Ellie Nia <98709814+Elliemnia@users.noreply.github.com>
2022-02-16 13:30:28 -0800

    Add files via upload

---
+++ Assignment_Array/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Balancebtl = New System.Windows.Forms.Button()
+        Me.BankAssetbtl = New System.Windows.Forms.Button()
+        Me.BalanceUnderbtl = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Balancebtl
+        '
+        Me.Balancebtl.Location = New System.Drawing.Point(109, 39)
+        Me.Balancebtl.Name = "Balancebtl"
+        Me.Balancebtl.Size = New System.Drawing.Size(181, 77)
+        Me.Balancebtl.TabIndex = 0
+        Me.Balancebtl.Text = "Show Balance"
+        Me.Balancebtl.UseVisualStyleBackColor = True
+        '
+        'BankAssetbtl
+        '
+        Me.BankAssetbtl.Location = New System.Drawing.Point(110, 144)
+        Me.BankAssetbtl.Name = "BankAssetbtl"
+        Me.BankAssetbtl.Size = New System.Drawing.Size(180, 83)
+        Me.BankAssetbtl.TabIndex = 1
+        Me.BankAssetbtl.Text = "Show Bank Assets"
+        Me.BankAssetbtl.UseVisualStyleBackColor = True
+        '
+        'BalanceUnderbtl
+        '
+        Me.BalanceUnderbtl.Location = New System.Drawing.Point(110, 257)
+        Me.BalanceUnderbtl.Name = "BalanceUnderbtl"
+        Me.BalanceUnderbtl.Size = New System.Drawing.Size(179, 81)
+        Me.BalanceUnderbtl.TabIndex = 2
+        Me.BalanceUnderbtl.Text = "Show Balance Under $500"
+        Me.BalanceUnderbtl.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(402, 389)
+        Me.Controls.Add(Me.BalanceUnderbtl)
+        Me.Controls.Add(Me.BankAssetbtl)
+        Me.Controls.Add(Me.Balancebtl)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Balancebtl As Button
+    Friend WithEvents BankAssetbtl As Button
+    Friend WithEvents BalanceUnderbtl As Button
+End Class
---
+++ Assignment_Array/Form1.vb
@@ -0,0 +1,62 @@
+﻿Public Class Form1
+    'Declare an arrays with value 
+    Dim Name_first() As String = {"jack", "marjy", "tom", "luke", "sam", "al", "joe", "miky", "lu"}
+    Dim Name_last() As String = {"jones", "ety", "fan", "spence", "amin", "sid", "bud", "ant", "ben"}
+    Dim Amount() As Double = {234.45, 8293.1, 943.25, 27381.0, 271.39, 7436.12, 2743.0, 1639.95, 2354.2}
+    Private Sub Balancebtl_Click(sender As Object, e As EventArgs) Handles Balancebtl.Click
+        'Declare variables to find the match
+        Dim intCount As Integer = 0
+        Dim blnFound As Boolean = False
+        Dim intPosition As Integer = -1
+        Dim Usertype As String
+        Dim match As Double
+        'Get the name from user
+        Usertype = InputBox("Type the client first name")
+
+        'Do the procces to find the match name in array
+        Do While Not blnFound And intCount < Name_first.Length
+            For intCount = 0 To Amount.Length - 1
+                If Name_first(intCount) = Usertype Then
+                    blnFound = True
+                    intPosition = intCount
+                    match = Amount(intCount)
+                End If
+            Next
+
+            intCount += 1
+        Loop
+        'if found the match name inside the array show the massage
+        If blnFound Then
+            MessageBox.Show("The balance of the " & Usertype & " is: " & match)
+        Else
+            MessageBox.Show("your name have not balance") ' if not found show the message
+        End If
+    End Sub
+
+    Private Sub BankAssetbtl_Click(sender As Object, e As EventArgs) Handles BankAssetbtl.Click
+        'Declare the variables we need, for total 
+        Dim total As Double = 0.0
+        Dim intvalue As Integer
+        'total the value in the amount 
+        For Each intvalue In Amount
+            total = total + intvalue
+
+        Next
+        MessageBox.Show("The total of all amount is: " & total)
+    End Sub
+
+    Private Sub BalanceUnderbtl_Click(sender As Object, e As EventArgs) Handles BalanceUnderbtl.Click
+        'Declare variable for keep the min number and step the element
+        Dim intcount As Integer
+        Dim intmin As Double
+        'check the elements of amount array to find the number under 500
+        For intcount = 0 To (Amount.Length - 1)
+            If Amount(intcount) < 500 Then
+                intmin = Amount(intcount)
+                MessageBox.Show("First name: " & Name_first(intcount) & " Last Name: " & Name_last(intcount) & " Amount: " & intmin)
+            End If
+
+
+        Next
+    End Sub
+End Class
